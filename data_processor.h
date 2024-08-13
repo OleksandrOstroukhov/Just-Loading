2c4f3081c937ff41fbf64bed1de310a6115d9e9a
Oleksandr <149805345+OleksandrOstroukhov@users.noreply.github.com>
2024-08-13 19:48:25 +0200

    Create data_processor.h

---
+++ data_processor.h
@@ -0,0 +1,14 @@
+#ifndef DATA_PROCESSOR_H
+#define DATA_PROCESSOR_H
+
+class DataProcessor {
+public:
+    void loadData();
+    void processData();
+    void saveData();
+
+private:
+    // Добавьте необходимые поля для обработки данных
+};
+
+#endif
